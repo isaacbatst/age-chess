53dfbcddb1c8cb3bcd6dea8e42ee950624683179
Isaac Batista <isaac.batst@gmail.com>
2021-02-18 21:55:29 -0600

    install redux

--- src/components/Board/Hexagon.js
+++ src/components/Board/Hexagon.js
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import { Text } from 'react-hexgrid-with-context-api'
 import { StyledHex } from './styles';
 
-
-
 function Hexagon(props) {
   const [ selected, setSelected ] = useState(false);
   const { hex, hex: { coordinates: { q, r, s }, icon = "", owner = "none", index }, onClick } = props;
   
-  return <StyledHex q={q} r={r} s={s} fill={icon} owner={owner} onClick={() => onClick(hex)} selected={selected}>
+  return <StyledHex q={q} r={r} s={s} fill={icon} owner={owner} onClick={() => {
+    onClick(hex);
+    setSelected(!selected)
+  }} selected={selected}>
     <Text>{index.toString()}</Text>
   </StyledHex>
 }
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -8,6 +8,7 @@ import spear from '../../assets/icons/spear.svg';
 import sword from '../../assets/icons/sword.svg';
 import horse from '../../assets/icons/horse.svg';
 
+
 function Board() {
   const [hexagons, setHexagons] = useState(defaultHexagons);
   const [selectedHexagon, setSelectedHexagon] = useState(null);
@@ -18,8 +19,6 @@ function Board() {
   function handleHexClick({ index }) {
     setSelectedHexagon(index);
   }
-
-  console.log(selectedHexagon)
 
   return <HexGrid width={config.width} height={config.height}>
     <StyledLayout size={size} flat={layout.flat} spacing={layout.spacing} origin={config.origin}>
