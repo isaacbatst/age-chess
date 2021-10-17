a1c69956fbf0fc2c4689d79aeb921cec2d993241
isaacbatst <isaacbatst@gmail.com>
2021-10-17 00:57:49 -0300

    improve styles

--- src/data/Board/config.js
+++ src/data/Board/config.js
@@ -1,7 +1,7 @@
 const gridConfig = {
   "width": 700,
-  "height": 500,
-  "layout": { "width": 9, "height": 9, "flat": false },
+  "height": 600,
+  "layout": { "width": 8, "height": 8, "flat": false, spacing: 1.1 },
   "origin": { "x": 0, "y": 0 },
   "map": "hexagon",
   "mapProps": [3]
